e6e6f7ab66ebd80f14fddd69a7bee1d0c52b7b92
yen.dinhhai <dinhyensamagada98@gmail.com>
2020-11-28 11:05:43 +0700

    modify: display list notes, add button ADD NOTE

--- lib/ui/views/note/note_view.dart
+++ lib/ui/views/note/note_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mynote/ui/views/note/note_model.dart';
 import 'package:mynote/ui/views/note/note_viewmodel.dart';
 import 'package:stacked/stacked.dart';
 
@@ -8,8 +9,23 @@ class NoteView extends StatelessWidget {
     return ViewModelBuilder<NoteViewModel>.reactive(
       builder: (context, model, child) => Scaffold(
         appBar: AppBar(title: Text(model.title)),
-        body: Center(
-          child: Text('Hello My Note'),
+        body: SingleChildScrollView(
+          child: ListView.builder(
+            scrollDirection: Axis.vertical,
+            shrinkWrap: true,
+            itemCount: model.items.length,
+            itemBuilder: (context, int index) {
+              Note item = model.items[index];
+              return ListTile(
+                title: Text(item.title),
+                subtitle: Text(item.desc),
+              );
+            },
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: null,
         ),
       ),
       viewModelBuilder: () => NoteViewModel(),
--- lib/ui/views/note/note_viewmodel.dart
+++ lib/ui/views/note/note_viewmodel.dart
@@ -1,5 +1,9 @@
+import 'package:mynote/ui/views/note/note_model.dart';
 import 'package:stacked/stacked.dart';
 
 class NoteViewModel extends BaseViewModel {
   final title = 'Note View Model';
+  var _items = <Note>[];
+
+  List<Note> get items => _items;
 }
